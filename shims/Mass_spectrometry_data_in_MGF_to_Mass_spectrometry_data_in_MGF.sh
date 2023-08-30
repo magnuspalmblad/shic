7e1e73b27394239b9afd3573752a0f30f6bd061b
Magnus Palmblad <n.m.palmblad@lumc.nl>
2023-08-30 16:36:14 +0200

    Update Mass_spectrometry_data_in_MGF_to_Mass_spectrometry_data_in_MGF.sh

--- shims/Mass_spectrometry_data_in_MGF_to_Mass_spectrometry_data_in_MGF.sh
+++ shims/Mass_spectrometry_data_in_MGF_to_Mass_spectrometry_data_in_MGF.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 awk 'BEGIN {FS="[ =/+]"}
-(NR==1) {title_filename=(FILENAME~/.mgf$/)?substr(FILENAME,1,length(FILENAME)-4):FILENAME}
-($1=="###MSMS:") {first_scan=$2; last_scan=$(NF-1)}
-($1!="TITLE") {print $0}
-($1=="CHARGE") {charge=$2; printf("TITLE=%s.%i.%i.%i\n",title_filename,first_scan,last_scan,charge)}
-{next}' $1 > $2
+     (NR==1) {title_filename=(FILENAME~/.mgf$/)?substr(FILENAME,1,length(FILENAME)-4):FILENAME}
+     ($1=="###MSMS:") {first_scan=$2; last_scan=$(NF-1)}
+     ($1!="TITLE") {print $0}
+     ($1=="CHARGE") {charge=$2; printf("TITLE=%s.%i.%i.%i\n",title_filename,first_scan,last_scan,charge)}
+     {next}
+' $1 > $2
